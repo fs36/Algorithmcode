857300fb91c41f1816588d1c53c5f657bb4539cb
fs36 <121793009+fs36@users.noreply.github.com>
2025-07-25 13:57:47 +0000

    feat:finish twoSum.

---
+++ Algorithm/twoSum.js
@@ -0,0 +1,15 @@
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 找出nums里面和为target的两个数，并返回其数组下标
+const nums = [2,7,11,15], target = 9
+function twoSum(nums,target){
+    const map = new Map()
+    for(let i=0;i<nums.length;i++){
+        if(map.has(target-nums[i])){
+            return [i,map.get(target-nums[i])]
+        }
+        map.set(nums[i],i)
+    }
+}
+const result = twoSum(nums,target)
+console.log(result)
