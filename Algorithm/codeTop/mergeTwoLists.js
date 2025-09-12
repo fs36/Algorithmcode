00f201b1323caf5d28bb29584d8626824e7b5ee2
zxw <1935661782@qq.com>
2025-09-12 16:03:16 +0800

    feat:9.12学习

---
+++ Algorithm/codeTop/mergeTwoLists.js
@@ -0,0 +1,30 @@
+// 21.合并两个有序列表
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+function mergeTwoLists(l1, l2) { 
+    let newList = new ListNode(0)
+    let curr = newList
+    while (l1 && l2) {
+        if(l1.val < l2.val) {
+            curr.next = l1
+            l1 = l1.next
+        } else {
+            curr.next = l2
+            l2 = l2.next
+        }
+        curr = curr.next
+    }
+    curr.next = l1 ? l1 : l2
+    return newList.next
+}
